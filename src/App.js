bcbc6df6d5ccec92b3bf9551972b8a4415d2f2bd
masturdating1991 <aligodosi@gmail.com>
2020-10-05 19:09:11 +0330

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import ReactJkMusicPlayer from 'react-jinke-music-player'
+import 'react-jinke-music-player/assets/index.css'
+import shapeOfYou from './Music/shape.mp3'
+import musicCover from './Music/shape.jpg'
+// https://www.npmjs.com/package/react-jinke-music-player
 
 function App() {
+  const audioList = [
+    {
+      cover: musicCover,
+      musicSrc: shapeOfYou,
+      name: 'Shape of you',
+      singer: 'Ed Sheeran'
+    }
+  ]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ReactJkMusicPlayer audioLists={audioList} autoPlay={false} toggleMode={false} mode="full" />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
